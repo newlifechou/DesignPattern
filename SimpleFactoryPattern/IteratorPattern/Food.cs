82e22ab51767b291c0d5632612271675a65e24b2
xs.zhou <newlifechou@outlook.com>
2018-04-19 03:13:49 +0800

    iterator pattern

---
+++ SimpleFactoryPattern/IteratorPattern/Food.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorPattern
+{
+    class Food : IEnumerator<int>
+    {
+        public string[] foods;
+        public Food(string[] foods)
+        {
+            this.foods = foods;
+        }
+        int position = -1;
+        public int Current => position;
+
+        object IEnumerator.Current => foods[position];
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MoveNext()
+        {
+            position++;
+            return position < foods.Length;
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+    }
+}
---
+++ SimpleFactoryPattern/IteratorPattern/Foods.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorPattern
+{
+    class Foods : IEnumerable<string>
+    {
+        private string[] foods = new string[10];
+        public Foods()
+        {
+            for (int i = 0; i < foods.Length; i++)
+            {
+                foods[i] = "food " + i;
+            }
+        }
+        public IEnumerator<string> GetEnumerator()
+        {
+            foreach (var food in foods)
+            {
+                yield return food;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            foreach (var food in foods)
+            {
+                yield return food;
+            }
+        }
+    }
+}
